90a33a45558696ae1cf6361e0225c15468309920
Kevin <kevinhadu.12@gmail.com>
2025-02-11 19:04:36 -0500

    commit trabajo 5

---
+++ Ej_propuesto_18/src/ej_propuesto_18/Ej_propuesto_18.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej_propuesto_18;
+
+/**
+ *
+ * @author ADMON
+ */
+public class Ej_propuesto_18 {
+
+    public static void main(String[] args) {
+        UI miventana = new UI();
+        miventana.setVisible(true);    }
+    
+}
---
+++ Trabajo 5/Ejercicio_8.3/src/Figuras/Main.java
@@ -0,0 +1,12 @@
+package Figuras;
+
+public class Main {
+
+        public static void main(String[] args) {
+            VentanaPrincipal miVentanaPrincipal;
+
+            miVentanaPrincipal= new VentanaPrincipal();
+            miVentanaPrincipal.setVisible(true); 
+            miVentanaPrincipal.setResizable(false);
+         }   
+}
